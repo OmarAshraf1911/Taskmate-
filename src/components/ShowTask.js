5f581a1d54a04012c571a59de181458791feb18a
OmarAshraf1911 <omarashraf.90.5.370@gmail.com>
2024-05-17 03:38:32 +0300

    Upload files

--- src/components/ShowTask.js
+++ src/components/ShowTask.js
@@ -1,9 +1,17 @@
-export const ShowTask = ({ tasklist, setTasklist }) => {
+export const ShowTask = ({ tasklist, setTasklist, task, setTask }) => {
   // const tasks = [
   //   { id: 1, name: "TASK1", time: "2:09:01 AM 9/12/2020" },
   //   { id: 2, name: "TASK2", time: "2:09:01 AM 9/12/2020" },
   //   { id: 3, name: "TASK3", time: "2:09:01 AM 9/12/2020" },
   // ];
+  const handleDelete = (id) => {
+    const updatedTaskList = tasklist.filter((todo) => todo.id !== id);
+    setTasklist(updatedTaskList);
+  };
+  const handleEdit = (id) => {
+    const selectTask = tasklist.find((todo) => todo.id === id);
+    setTask(selectTask);
+  };
   return (
     <section className="showTask">
       <div className="head">
@@ -11,17 +19,25 @@ export const ShowTask = ({ tasklist, setTasklist }) => {
           <span className="title">Todo</span>
           <span className="count">{tasklist.length}</span>
         </div>
-        <button className="clearAll">Clear All</button>
+        <button onClick={() => setTasklist([])} className="clearAll">
+          Clear All
+        </button>
       </div>
       <ul>
-        {tasklist.map((task) => (
-          <li key={task.id}>
+        {tasklist.map((todo) => (
+          <li key={todo.id}>
             <p>
-              <span className="name">{task.name}</span>
-              <span className="time">{task.time}</span>
+              <span className="name">{todo.name}</span>
+              <span className="time">{todo.time}</span>
             </p>
-            <i className="bi bi-pencil-square"></i>
-            <i className="bi bi-trash"></i>
+            <i
+              onClick={() => handleEdit(todo.id)}
+              className="bi bi-pencil-square"
+            ></i>
+            <i
+              onClick={() => handleDelete(todo.id)}
+              className="bi bi-trash"
+            ></i>
           </li>
         ))}
       </ul>
